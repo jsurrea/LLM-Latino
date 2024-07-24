fa9545e92347eb3fec3aea0ce4c0c18e9951ccd3
Sebastian Urrea <js.urrea@uniandes.edu.co>
2024-07-23 23:27:01 -0500

    Add UUIDs per faculty

--- src/main.py
+++ src/main.py
@@ -58,7 +58,22 @@ def fetch_data(uuids, max_pages=-1):
     df.to_csv('downloads/repositorio_uniandes_data.csv', index=False)
 
 uuids = [
-    '4b005e8d-5527-46a9-bbf0-61ce879432b0' # Ingeniería
+    '9d6d8fb8-dfc8-4f12-9689-90922ec6f946', # Administración
+    '0253089e-b02f-4c0b-91f6-26d29f64febb', # Arquitectura y Diseño
+    'ba3138b7-1e4c-42f9-b462-163936574e64', # Artes y Humanidades
+    'f756e30f-8cb4-4090-8d9a-9ffb0f6bab10', # Centro Interdisciplinario de Estudios sobre Desarrollo - CIDER
+    'd3c988ec-ea05-484d-a128-da0dbaa52aee', # Ciencias
+    'f21012c9-3539-4efd-98ad-f9cabeb446a5', # Ciencias Sociales
+    '95b30f28-97de-4a4e-b1f3-3dc71fce1ef0', # Derecho
+    '2664c78e-aadd-4c67-815d-aa201c3215b1', # Economía
+    '196f68f5-feba-4108-9143-2501834d732d', # Educación
+    '811559f7-8706-40a0-82ba-f37cf856bf19', # Escuela de Gobierno Alberto Lleras Camargo
+    '4b005e8d-5527-46a9-bbf0-61ce879432b0', # Ingeniería
+    '9a8975c9-fcae-4049-a6d5-0bb3f5e5db23', # Medicina
+    'bee8fbe6-846f-41e9-8202-e73ea7754aab', # Nodo de Innovación
+    'fedfccbb-9f0a-4848-bb7b-20c4f3146fb8', # Secretaría General
+    '4272016d-2779-4f32-96fb-e89fed4256e5', # Sistema de Bibliotecas
+    'e1e56670-a1c4-4431-b130-031ec2fd7dd2', # Observatorio Regional ODS
 ]
 
 fetch_data(uuids)  # Descarga las primeras 5 páginas
